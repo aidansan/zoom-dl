2f4a4c7f1c2584589a29be66a3408bbf77d0e3c3
Aidan San <aidan.w.san@gmail.com>
2024-08-03 00:10:29 -0400

    cleanup script a bit

--- script.py
+++ script.py
@@ -1,11 +1,9 @@
-# Do I need this? https://pypi.org/project/pyperclip/
-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver import ActionChains
 import selenium
-
+import time
 
 def scroll_click(elem, max_attempts=100000):
     for i in range(max_attempts):
@@ -14,8 +12,6 @@ def scroll_click(elem, max_attempts=100000):
             return
         except selenium.common.exceptions.ElementClickInterceptedException:
             ActionChains(browser).scroll_by_amount(0, 10).perform()
-
-import time
 
 LOGIN_TIME = 20
 MED_WAIT_TIME = 5
@@ -37,13 +33,6 @@ while True:
     share_btns = [b for b in share_btns if 'Share' in b.text]
 
     for share_btn in share_btns:
-        # ActionChains(browser)\
-        # .move_to_element(share_btn)\
-        # .perform()
-        # import pdb
-        # pdb.set_trace()
-        # browser.execute_script("arguments[0].scrollIntoView();", share_btn)
-
         scroll_click(share_btn)
         time.sleep(SMALL_WAIT_TIME)
         share_link_text = browser.find_element(By.CLASS_NAME, 'share-link').text
@@ -53,18 +42,8 @@ while True:
 
     next_page_btn = browser.find_element(By.CLASS_NAME, 'btn-next')
     if next_page_btn.is_enabled():
-        # import pdb
-        # pdb.set_trace()
         scroll_click(next_page_btn)
         time.sleep(SMALL_WAIT_TIME)
         browser.execute_script("window.scrollTo(0, document.body.scrollTop);")
     else:
         break
-
-import pdb
-pdb.set_trace()
-
-# elem = browser.find_element(By.NAME, 'p')  # Find the search box
-# elem.send_keys('seleniumhq' + Keys.RETURN)
-
-# browser.quit()
